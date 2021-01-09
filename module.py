609ced6cc1cd81162b4b5b6b37c4ab329cacf7a2
Goren Barak <gorenemail@gmail.com>
2021-01-08 20:44:28 -0500

    added method "ask"

--- module.py
+++ module.py
@@ -3,4 +3,6 @@ class Person:
         self.name = name
         self.age = age
     def talk(self,text):
-        print(f"{self.name.title()} said {text}")
+        print(f"{self.name.title()} said {text}.")
+    def ask(self,text):
+        print(f"{self.name.title()} asked {text}.")
